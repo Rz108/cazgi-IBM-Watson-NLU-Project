c92419c39f9efe32b20e793ccb37cc25a820c875
Rz108 <98604739+Rz108@users.noreply.github.com>
2022-02-08 18:46:52 +0800

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,77 +1,138 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = new express();
+dotenv.config();
 
-/*This tells the server to use the client 
-folder for all static resources*/
-app.use(express.static('client'));
+app.use(express.static('client'))
 
-/*This tells the server to allow cross origin references*/
 const cors_app = require('cors');
 app.use(cors_app());
 
-/*Uncomment the following lines to loan the environment 
-variables that you set up in the .env file*/
-
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-// const api_key = process.env.API_KEY;
-// const api_url = process.env.API_URL;
 
 function getNLUInstance() {
-    /*Type the code to create the NLU instance and return it.
-    You can refer to the image in the instructions document
-    to do the same.*/
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
 }
 
-
-//The default endpoint for the webserver
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index.html');
-  });
-
-//The endpoint for the webserver ending with /url/emotion
-app.get("/url/emotion", (req,res) => {
-    // //Extract the url passed from the client through the request object
-    // let urlToAnalyze = req.query.url
-    // const analyzeParams = 
-    //     {
-    //         "url": urlToAnalyze,
-    //         "features": {
-    //             "keywords": {
-    //                             "emotion": true,
-    //                             "limit": 1
-    //                         }
-    //         }
-    //     }
-     
-    //  const naturalLanguageUnderstanding = getNLUInstance();
-     
-    //  naturalLanguageUnderstanding.analyze(analyzeParams)
-    //  .then(analysisResults => {
-    //     //Please refer to the image to see the order of retrieval
-    //     return res.send(analysisResults.result.keywords[0].emotion,null,2);
-    //  })
-    //  .catch(err => {
-    //  return res.send("Could not do desired operation "+err);
-    //  });
 });
 
-//The endpoint for the webserver ending with /url/sentiment
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+    if (req.query.url != "") {
+        const analyzeParams = {
+            'url': req.query.url,
+            'features': {
+                'entities': {
+                    'emotion': true,
+                    'limit': 1
+                }
+            }
+        }
+
+        getNLUInstance().analyze(analyzeParams)
+            .then(analysisResults => {
+
+                //console.log(JSON.stringify(analysisResults.result.entities[0].emotion, null, 2));
+                return res.send(analysisResults.result.entities[0].emotion, null, 2);
+            }).catch(err => {
+                console.log('error', err);
+                return res.send("Error occured");
+            });
+    } else {
+        return res.send("Can't Analyze An Empty URL");
+    }
 });
 
-//The endpoint for the webserver ending with /text/emotion
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+
+app.get("/url/sentiment", (req, res) => {
+    if (req.query.url != "") {
+        const analyzeParams = {
+            'url': req.query.url,
+            'features': {
+                'entities': {
+                    'sentiment': true,
+                    'limit': 1
+                }
+            }
+        }
+
+        getNLUInstance().analyze(analyzeParams)
+            .then(analysisResults => {
+                // console.log(JSON.stringify(analysisResults.result.entities[0].sentiment.label));
+                return res.send(analysisResults.result.entities[0].sentiment.label, null, 2);
+            }).catch(err => {
+                console.log('error:', err);
+                return res.send("Error occured");
+            })
+    } else {
+        return res.send("Can't Analyze An Empty URL");
+    }
+
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+    if (req.query.text != "") {
+
+        const analyzeParams = {
+            'text': req.query.text,
+            'features': {
+                'emotion': {}
+            }
+        }
+        getNLUInstance().analyze(analyzeParams)
+            .then(analysisResults => {
+                // console.log(analysisResults)
+                // e = analysisResults.result.emotion.document.emotion;
+                // return res.send(e);
+                //console.log(JSON.stringify(analysisResults.result.emotion.document.emotion, null, 2));
+                return res.send(analysisResults.result.emotion.document.emotion);
+            }).catch(err => {
+                console.log('error', err);
+                return res.send("Error occured");
+            });
+    } else {
+        return res.send("Can't Analyze Empty Input");
+    }
 });
 
-let server = app.listen(8080, () => {
-    console.log('Listening', server.address().port)
-})
+app.get("/text/sentiment", (req, res) => {
 
+    if (req.query.text != "") {
+
+        const analyzeParams = {
+            'text': req.query.text,
+            'features': {
+                'sentiment': {}
+            }
+        };
+
+        getNLUInstance().analyze(analyzeParams)
+            .then(analysisResults => {
+                s = analysisResults.result.sentiment.document.label;
+                //console.log(s);
+                return res.send(s);
+            }).catch(err => {
+                console.log('error', err);
+                return res.send("Error occured");
+            });
+    } else {
+        return res.send("Can't Analyze Empty Input");
+    }
+});
+
+app.get("/", (req, res) => {
+    res.render('index.html');
+});
